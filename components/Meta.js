6824864bd16ac66d394e24ceb745704b622ab882
Khawar Jatoi <khawarjatoi@live.com>
2024-06-13 15:22:38 +0500

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -10,6 +10,7 @@ const Meta = () => (
       name='description'
       content="Khawar Jatoi's site"
     ></meta>
+    <meta name="cf-2fa-verify" content="e10966b10e9dcea" />
     <link rel='shortcut icon' href='/KJ-blackpink.png' />
     <title>
       Full-Stack Web Developer | Khawar Jatoi | JavaScript, Node.js, React.js
